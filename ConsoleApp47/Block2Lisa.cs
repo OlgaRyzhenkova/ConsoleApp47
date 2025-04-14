1b94fe4f70ff9aa9e72f08dfef6a404cc113312e
Liza Bondarenko <bondarenkoliza705@gmail.com>
2025-04-15 01:04:23 +0300

    був додан блок 2 з простою заміною літер

--- ConsoleApp47/Block2Lisa.cs
+++ ConsoleApp47/Block2Lisa.cs
@@ -1,15 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp47
+namespace ProjectForLaba4
 {
-    partial class Methods
+    class MenyBondBlockSecond
     {
-        public static void Block2Lisa()
-        { 
+        public static bool MenyForBondarenkoSecondBlock()
+        {
+            bool stayInSubMenu = true;
+            while (stayInSubMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("Меню Завдань Лізи (Блок 2):");
+                Console.WriteLine("1. Виконати методу з простою заміною ");
+                Console.WriteLine("2. Виконати метод 2 ");
+                Console.WriteLine("3. Виконати метод 3 ");
+                Console.WriteLine("6. Вийти З УСІЄЇ ПРОГРАМИ");
+                Console.WriteLine("7. Повернутись до головного меню");
+                Console.Write("Виберіть опцію (1-7): ");
+
+                if (!int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    Console.WriteLine("Некоректне введення.");
+                }
+                else
+                {
+                    Console.Clear();
+
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Результат методу з простою заміною:");
+                            SimpleCode.VayBlock();
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;
+                        case 6:
+                            stayInSubMenu = false;
+                            Console.WriteLine("Вихід з програми...");
+                            return false;
+                        case 7:
+                            stayInSubMenu = false;
+                            Console.WriteLine("Повернення до головного меню...");
+                            return true;
+                        default:
+                            Console.WriteLine("Неправильний вибір. Спробуйте ще раз.");
+                            break;
+                    }
+                }
+            }
+            return true;
         }
     }
 }
