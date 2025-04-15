41135bb39bcf965abbbc7fb7ecd41ae18a3823e1
Liza Bondarenko <bondarenko.yelyzaveta1124@vu.cdu.edu.ua>
2025-04-15 20:41:33 +0300

    зробила перше додаткове завдання та меню для цього блоку завдань

---
+++ ConsoleApp47/Dop1Liza.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace ProjectForLaba4
+{
+    class Dop15Liza
+    {
+        public static void DopLizaFirst()
+        {
+            Console.WriteLine("Введіть рядок тексту:");
+            string input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Ви не ввели текст.");
+                return;
+            }
+
+            string correctedOutput = Parentheses(input);
+            Console.WriteLine("\nВиправлений текст:");
+            Console.WriteLine(correctedOutput);
+
+            Console.WriteLine("\nНатисніть Enter, щоб продовжити...");
+            Console.ReadLine();
+        }
+        static string Parentheses(string text)
+        {
+            StringBuilder result = new StringBuilder();
+            int count = 0;
+            foreach (char c in text)
+            {
+                if (c == '(')
+                {
+                    result.Append(c);
+                    count++;
+                }
+                else if (c == ')')
+                {
+                    if (count > 0)
+                    {
+                        result.Append(c);
+                        count--;
+                    }
+                }
+                else result.Append(c);
+            }
+            for (int i = 0; i < count; i++)
+            {
+                result.Append(')');
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ ConsoleApp47/DopMenyDodatkovi.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectForLaba4
+{
+    class MenyBondDodatkovi
+    {
+        public static bool MenyForBondarenkoDodatkovi()
+        {
+            bool stayInSubMenu = true;
+            while (stayInSubMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("Меню Завдань Лізи (Додаткові завдання):");
+                Console.WriteLine("1. Виконати додаткове завдання 1 ");
+                Console.WriteLine("2. Виконати додаткове завдання 2 ");
+                Console.WriteLine("3. Виконати додаткове завдання 3");
+                Console.WriteLine("4. Вийти З УСІЄЇ ПРОГРАМИ");
+                Console.WriteLine("5. Повернутись до головного меню");
+                Console.Write("Виберіть опцію (1-7): ");
+
+                if (!int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    Console.WriteLine("Некоректне введення.");
+                }
+                else
+                {
+                    Console.Clear();
+
+                    switch (choice)
+                    {
+                        case 1:
+                            Dop15Liza.DopLizaFirst();
+                            break;
+                        case 2:
+                           // NotSimpleCode.NotVayBlock();
+                            break;
+                        case 3:
+                            //NotSimpleCode.NotVayBlock();
+                            break;
+                        case 4:
+                            stayInSubMenu = false;
+                            Console.WriteLine("Вихід з програми...");
+                            return false;
+                        case 5:
+                            stayInSubMenu = false;
+                            Console.WriteLine("Повернення до головного меню...");
+                            return true;
+                        default:
+                            Console.WriteLine("Неправильний вибір. Спробуйте ще раз.");
+                            break;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
