a1c4bc50ec51a511ce06604e2a9af03f0404fdf3
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2025-04-16 10:06:26 +0300

    Змінила додаткові завдання

--- ConsoleApp47/Dop/DodExerOlia.cs
+++ ConsoleApp47/Dop/DodExerOlia.cs
@@ -12,7 +12,6 @@ namespace ConsoleApp47
                 Console.WriteLine("Виберіть завдання, яке потрібно виконати:");
                 Console.WriteLine("1. Завдання 1");
                 Console.WriteLine("2. Завдання 2");
-                Console.WriteLine("3. Завдання 3");
                 Console.WriteLine("0. Вихід");
                 Console.Write("Ваш вибір: ");
                 string choice = Console.ReadLine();
@@ -23,9 +22,6 @@ namespace ConsoleApp47
                         break;
                     case "2":
                         Task2();
-                        break;
-                    case "3":
-                        Task3();
                         break;
                     case "0":
                         Console.WriteLine("Вихід з програми");
@@ -108,126 +104,6 @@ namespace ConsoleApp47
                 }
             }
             return "^" + regex.ToString() + "$";
-        }
-        static void Task3()
-        {
-            Console.WriteLine("Виконання завдання 3");
-            Console.Write("Введіть рядок: ");
-            string input = Console.ReadLine();
-            string[] parts = input.Split(' ');
-
-            for (int i = 0; i < parts.Length - 1; i++)
-            {
-                int number;
-                if (int.TryParse(parts[i], out number))
-                {
-                    string unit = parts[i + 1];
-                    if (unit == "м" || unit == "грн")
-                    {
-                        parts[i] = NumberToWords(number, unit);      
-                        parts[i + 1] = UnitToWords(number, unit);  
-                    }
-                }
-            }
-
-            Console.WriteLine("Результат: " + string.Join(" ", parts));
-        }
-
-        static string NumberToWords(int number, string unit)
-        {
-            string[] onesMasculine = { "", "один", "два", "три", "чотири", "п’ять", "шість", "сім", "вісім", "дев’ять" };
-            string[] onesFeminine = { "", "одна", "дві", "три", "чотири", "п’ять", "шість", "сім", "вісім", "дев’ять" };
-            string[] teens = { "десять", "одинадцять", "дванадцять", "тринадцять", "чотирнадцять", "п’ятнадцять", "шістнадцять", "сімнадцять", "вісімнадцять", "дев’ятнадцять" };
-            string[] tens = { "", "", "двадцять", "тридцять", "сорок", "п’ятдесят", "шістдесят", "сімдесят", "вісімдесят", "дев’яносто" };
-            string[] hundreds = { "", "сто", "двісті", "триста", "чотириста", "п’ятсот", "шістсот", "сімсот", "вісімсот", "дев’ятсот" };
-
-            string TripletToWords(int n, bool isFeminine)
-            {
-                string[] ones = isFeminine ? onesFeminine : onesMasculine;
-                int h = n / 100;
-                int t = (n / 10) % 10;
-                int o = n % 10;
-
-                string result = hundreds[h];
-
-                if (t == 1)
-                    result += " " + teens[o];
-                else
-                {
-                    if (t > 1) result += " " + tens[t];
-                    if (o > 0) result += " " + ones[o];
-                }
-
-                return result.Trim();
-            }
-
-            int billions = number / 1_000_000_000;
-            int millions = (number / 1_000_000) % 1000;
-            int thousands = (number / 1000) % 1000;
-            int rest = number % 1000;
-
-            string words = "";
-
-            if (billions > 0)
-            {
-                words += TripletToWords(billions, false) + " " + GetForm(billions, "мільярд") + " ";
-            }
-            if (millions > 0)
-            {
-                words += TripletToWords(millions, false) + " " + GetForm(millions, "мільйон") + " ";
-            }
-            if (thousands > 0)
-            {
-                words += TripletToWords(thousands, true) + " " + GetForm(thousands, "тисяча") + " ";
-            }
-            if (rest > 0 || number == 0)
-            {
-                words += TripletToWords(rest, unit == "грн");
-            }
-
-            return words.Trim();
-        }
-
-        static string UnitToWords(int number, string unit)
-        {
-            int lastDigit = number % 10;
-            int lastTwo = number % 100;
-
-            if (unit == "м")
-            {
-                if (lastDigit == 1 && lastTwo != 11) return "метр";
-                else if (lastDigit >= 2 && lastDigit <= 4 && !(lastTwo >= 12 && lastTwo <= 14)) return "метри";
-                else return "метрів";
-            }
-            else if (unit == "грн")
-            {
-                if (lastDigit == 1 && lastTwo != 11) return "гривня";
-                else if (lastDigit >= 2 && lastDigit <= 4 && !(lastTwo >= 12 && lastTwo <= 14)) return "гривні";
-                else return "гривень";
-            }
-
-            return unit;
-        }
-
-        static string GetForm(int number, string baseWord)
-        {
-            int lastDigit = number % 10;
-            int lastTwo = number % 100;
-
-            if (baseWord == "мільйон" || baseWord == "мільярд")
-            {
-                if (lastDigit == 1 && lastTwo != 11) return baseWord;
-                else if (lastDigit >= 2 && lastDigit <= 4 && !(lastTwo >= 12 && lastTwo <= 14)) return baseWord + "и";
-                else return baseWord + "ів";
-            }
-            else if (baseWord == "тисяча")
-            {
-                if (lastDigit == 1 && lastTwo != 11) return "тисяча";
-                else if (lastDigit >= 2 && lastDigit <= 4 && !(lastTwo >= 12 && lastTwo <= 14)) return "тисячі";
-                else return "тисяч";
-            }
-
-            return baseWord;
         }
     }
 }
