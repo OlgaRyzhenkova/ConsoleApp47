a5b0e8bfc07b28d2b292327844b99922284bc759
Volvach Anastasia <avsiaa@icloud.com>
2025-04-22 14:30:34 +0300

    прибрала зайвий if в першому блоці

--- ConsoleApp47/Nastia/Block1Nastia.cs
+++ ConsoleApp47/Nastia/Block1Nastia.cs
@@ -9,62 +9,40 @@ namespace ConsoleApp47
         {
             string output1 = "";
             for (int i = 1; i <= n; i++)
-            {
-                output1 += i;
-                if (i < n)
                 {
-                    output1 += " ";
+                    output1 += i + " ";
                 }
-            }
-            return output1;
+            return output1.Trim();
         }
 
         public static string Input2(int n)
         {
             string output2 = "";
             for (int i = n; i >= 1; i--)
-            {
-                if (output2 == "")
-                {
-                    output2 = i.ToString();
-                }
-                else
                 {
                     output2 = i + " " + output2;
                 }
-            }
-            return output2;
+            return output2.Trim();
         }
 
         public static string Input3(int n)
         {
             StringBuilder output3 = new StringBuilder();
             for (int i = 1; i <= n; i++)
-            {
-                output3.Append(i);
-                if (i < n)
                 {
-                    output3.Append(" ");
+                    output3.Append(i).Append(" ");
                 }
-            }
-            return output3.ToString();
+            return output3.ToString().Trim();
         }
 
         public static string Input4(int n)
         {
             StringBuilder output4 = new StringBuilder();
             for (int i = n; i >= 1; i--)
-            {
-                if (output4.Length == 0)
-                {
-                    output4.Insert(0, i);
-                }
-                else
                 {
                     output4.Insert(0, i + " ");
                 }
-            }
-            return output4.ToString();
+            return output4.ToString().Trim();
         }
     }
 }
